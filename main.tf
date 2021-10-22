cb8e8daea74a34ffcb26a250b92b2ca77afebe3a
cuacah <52966756+cuacah@users.noreply.github.com>
2021-10-21 19:35:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+module "s3-hen-webapp" {
+  source  = "app.terraform.io/henny-private/hen-s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
